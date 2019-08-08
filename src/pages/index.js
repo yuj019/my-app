a1d28295c92de504387f1635082ce5309487013b
Yumei Jin <yumeiisima.io@Yumeis-MacBook-Pro-2.local>
2019-08-07 20:45:03 -0700

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Learn to <br />design and code React apps</h1>
     <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <p>Now go build something great.</p>
+    <p>Stay tuned!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
