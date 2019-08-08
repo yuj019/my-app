4fed370ddd4d60c6c24607e8ecbf3e847e8c45f9
Yumei Jin <yumeiisima.io@Yumeis-MacBook-Pro-2.local>
2019-08-07 20:21:16 -0700

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Learn to <br />design and code React apps</h1>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
